69e60613d7754bf66fb01c6876776a8452ec4150
Max <143605086+maxim513@users.noreply.github.com>
2023-11-15 08:28:44 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,14 @@
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
         print(f"Random Number: {item}")
 
-import random
-if __name__ == "__main__":
-    data = generate_random_data()
-    main()
     return data
-    data = [random.randint(1, 100) for _ in range(10)]
+
+
 def main():
-
-def generate_random_data():
-
+import random
+    main()
     for item in data:
+if __name__ == "__main__":
+def generate_random_data():
