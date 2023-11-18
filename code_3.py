4a895be2fc9a8ff371c763f8c0434697d9477ad5
Max <143605086+maxim513@users.noreply.github.com>
2023-11-18 03:42:12 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,14 +1,29 @@
+main();
+        console.log("Random Number: " + item);
 
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-        print(f"Random Number: {item}")
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+    return data;
 
+    });
+}
+    const data = generateRandomData();
+function main() {
+
+function generateRandomData() {
+    }
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];    data = [random.randint(1, 100) for _ in range(10)]
+    main()
     return data
 
-
-def main():
 import random
-    main()
-    for item in data:
 if __name__ == "__main__":
+
+    for item in data:
 def generate_random_data():
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+def main():
+
