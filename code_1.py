a0662bb1e1742cacd350c4115c21693ac0f91817
Max <143605086+maxim513@users.noreply.github.com>
2023-11-23 18:08:39 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,30 +1,68 @@
+import random
+def main():
+    data = generate_random_data()
+    for item in data:
+
+
+
+
+    return data
+        print(f"Random Number: {item}")
+    main()
     data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+if __name__ == "__main__":
+import random
+
+if __name__ == "__main__":
 
         print(f"Random Number: {item}")
-    return data
-
-def generate_random_data():
-def main():
     main()
 
+def main():
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    data = generate_random_data()
+def generate_random_data():def main():
+    data = generate_random_data()
+    return data
+
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
 
     for item in data:
-if __name__ == "__main__":
-    data = generate_random_data()
-import random        data.push(Math.floor(Math.random() * 100) + 1);
 
-    const data = generateRandomData();
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+import random
 main();
     return data;
-    const data = [];
-    }
 
     for (let i = 0; i < 10; i++) {
-function main() {
-        console.log("Random Number: " + item);
-    data.forEach(item => {
-function generateRandomData() {
-
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
     });
+    const data = [];
+
+    const data = generateRandomData();
+function generateRandomData() {
+        console.log("Random Number: " + item);
 }
+    }
+
+    data.forEach(item => {
+function main() {    }
+public class RandomDataGenerator {
 }
+        }
+        for (int i = 0; i < 10; i++) {
+
+        for (int item : data) {
+
+        }
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        int[] data = new int[10];
