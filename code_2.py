ddf0eb6aeba7a86462627042b0a264f62a3a22fb
Max <143605086+maxim513@users.noreply.github.com>
2023-11-26 13:23:27 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,51 +1,43 @@
     main()
+def generate_random_data():
+
+
+
     for item in data:
-
-import random
     return data
-def generate_random_data():
-def main():
     data = [random.randint(1, 100) for _ in range(10)]
 
-    data = generate_random_data()
-if __name__ == "__main__":
         print(f"Random Number: {item}")
+if __name__ == "__main__":
+def main():
+    data = generate_random_data()
+import randomimport random
 
+    data = [random.randint(1, 100) for _ in range(10)]
     main()
+def generate_random_data():
+    for item in data:
 def main():
 
 
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-def generate_random_data():
     return data
-import random
 
-    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
     data = generate_random_data()
-
-    for item in data:            data[i] = (int) (Math.random() * 100) + 1;
-        for (int i = 0; i < 10; i++) {
-
-    public static void main(String[] args) {
-
-        for (int item : data) {
-        }
-}
-        int[] data = new int[10];
+if __name__ == "__main__":function main() {
     }
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-        }            data[i] = (int) (Math.random() * 100) + 1;
-        for (int item : data) {
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
 
+    const data = [];
+    });
 
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-        }
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    return data;
 }
-        }
-    }
-        int[] data = new int[10];
-        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
+    data.forEach(item => {
+}
+        console.log("Random Number: " + item);
+function generateRandomData() {
